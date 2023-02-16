6df2c1f239c9299a4a19d21681d03d4e24765038
Rakshita Gowda <101338848+Rakshita2003@users.noreply.github.com>
2023-02-16 19:54:59 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
+
 import React from 'react';
-import { Routes,Route } from 'react-router-dom';
+import List from './Component/List';
 import Home from './Component/Home';
 import About from './Component/About';
-import Services from './Component/Services';
-import Contact from './Component/Contact';
 import Reviews from './Component/Reviews';
-import List from './Component/List';
+import Contact from './Component/Contact';
 import Footer from './Component/Footer';
+import '../src/CSS/Home.css';
+
+
 const App=()=> {
   return (
     <>
-    <List/>
-    <Routes>
-      <Route exact path="/" element={<Home/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/services" element={<Services/>}/>
-      <Route path="/contact" element={<Contact/>}/>
-      <Route path="/reviews" element={<Reviews/>}/>
-    </Routes>
-    <Footer/>
+        <List/>
+        <Home/>
+        <About/>
+        <Reviews/>
+        <Contact/>
+        <Footer/>
+
     </>
   )
 }
