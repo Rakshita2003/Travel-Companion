55d48e68756f772279ba1bdf76fdefd4b37cc863
simran-022 <simransgupta22@gmail.com>
2023-02-18 18:59:50 +0530

    Updating About-us

--- src/Component/Reviews.js
+++ src/Component/Reviews.js
@@ -10,7 +10,7 @@ const Reviews = (props) => {
   return (
     <div>
       <div className="reviews">
-        <img src={orgrect} alt="Rectangle" className="rectangle"/>
+        <img src={orgrect} alt="Rectangle" className="rectangle1"/>
         <img src={vector} alt="Vector" className="vector"/>
         <span className="text">
           <span>Word from our customers</span>
@@ -20,11 +20,11 @@ const Reviews = (props) => {
         </div>
         <img src={rect2} alt="Rectangle2" className="rectangle2"/>
         <img src={pic} alt="jake" className="image"/>
-        <span className="text2">
+        <span className="text-2">
           <span> Really the best site for a happier journey.</span>
         </span>
         <img src={vector2} alt="Vector2" className="vector2"/>
-        <span className="text4">
+        <span className="text-4">
           <span>
             <span>Jake Sully</span>
             <br></br>
